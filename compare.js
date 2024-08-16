26a3b183293ceb5b3a993e71786d18f4ae6251f7
viratsingh <singhvirat1890@email.com>
2024-08-16 23:10:35 +0530

    add compare between pos and neg

---
+++ compare.js
@@ -0,0 +1,12 @@
+let a=8;
+a=Number.parseInt(a);
+if(a>0){
+    console.log("number is positive");
+}
+else if(a<0){
+    console.log("number is positive")
+}
+else if(a===0){
+    console.log("number is zero")
+
+}
